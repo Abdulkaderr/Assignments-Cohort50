086892ccdf2045ba3408d905a050a9ee8be60ebd
Abdul Kader <abdulkader@tikapp.link>
2024-10-15 16:49:23 +0200

    neww

--- 2-Browsers/Week1/assignment/ex1-bookList/index.js
+++ 2-Browsers/Week1/assignment/ex1-bookList/index.js
@@ -1,71 +1,65 @@
-//cspell: disable
+/cspell: disable
 /*------------------------------------------------------------------------------
 Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-Browsers/Week1#exercise-1-the-book-list
-
 I'd like to display my three favorite books inside a nice webpage!
-
 1. Iterate through the array of books.
 2. For each book, create a `<p>`
 element with the book title and author.
 3. Use a `<ul>`  and `<li>` to display the books.
 4. Add an `<img>` to each book that links to a URL of the book cover.
 5. Change the style of the book depending on whether you have read it(green) or not(red).
-
 The end result should look something like this:
 https://hackyourfuture.github.io/example-pages/Browsers/Week1/1-booklist/
-
 -----------------------------------------------------------------------------*/
 //cspell: enable
-
 function createBookList(books) {
   const ul = document.createElement('ul');
-
-books.foreach((book)=>{
-const li = document.createElement('li');
-const p = document.createElement('p');
-p.textContent = `${book.title} by ${book.author}`;
-li.appendChild(p);
-
-const images = document.createElement('img');
-images.src = `./assets ${book.img}`;
-images.alt = book.title;
-images.appendChild(li);
-li.style.backgroundColor = book.alreadyRead ? "Green" : "Red";
-ul.appendChild(li);
-});
-return ul;
+  books.forEach((book) => {
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    p.textContent = `${book.title} by ${book.author}`;
+    li.appendChild(p);
+    const img = document.createElement('img');
+    img.src = `./assets/${book.image}`;
+    img.alt = book.title;
+    li.appendChild(img);
+    li.style.backgroundColor = book.alreadyRead ? 'green' : 'red';
+    ul.appendChild(li);
+  });
+  return ul;
 }
-
 function main() {
   const myBooks = [
     {
       title: 'The Design of Everyday Things',
-      img: 'the_design_of_everyday_things.jpg',
       author: 'Don Norman',
       isbn: '978-0465050659',
+      image: 'the_design_of_everyday_things.jpg',
       alreadyRead: false,
     },
     {
       title: 'The Most Human Human',
-      img: 'the_most_human_human.jpg',
       author: 'Brian Christian',
       isbn: '978-1617933431',
+      image: 'the_most_human_human.jpg',
       alreadyRead: true,
     },
     {
       title: 'The Pragmatic Programmer',
-      img:'the_pragmatic_programmer.jpg',
       author: 'Andrew Hunt',
       isbn: '978-0201616224',
+      image: 'the_pragmatic_programmer.jpg',
       alreadyRead: true,
     },
   ];
-
   const ulElement = createBookList(myBooks);
   document.querySelector('#bookList').appendChild(ulElement);
 }
-
 window.addEventListener('load', main);
+
+
+
+
 
 
 
